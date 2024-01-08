67b6880c857bcfc42325d09d8cf4a996956b7b16
Can Dinler <candinler4@gmail.com>
2024-01-08 19:27:41 +0300

    Task Project
    Task Project

---
+++ TODO/todoapp/src/Header.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import AssignmentIcon from '@mui/icons-material/Assignment';
+
+function Header() {
+    return (
+        <header>
+            <h1>
+                <AssignmentIcon/>
+                To Do App
+            </h1>
+        </header>
+    )
+}
+
+
+
+export default Header;

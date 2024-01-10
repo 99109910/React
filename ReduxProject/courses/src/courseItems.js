8052a027b5f8639bab7b437da8bfdc1e05acc3a7
Can Dinler <candinler4@gmail.com>
2024-01-11 01:08:01 +0300

    Redux Toolkit Project
    Redux Toolkit Project

---
+++ ReduxProject/courses/src/courseItems.js
@@ -0,0 +1,32 @@
+const courseItems = [
+  {
+    id: 'adsadsadsadsada',
+    title: 'CSS',
+    price: '9',
+    img :'https://miro.medium.com/v2/resize:fit:750/format:webp/1*OFsc0SD55jhi8cjo7aCA4w.jpeg',
+    quantity: 1,
+  },
+  {
+    id: 'sadsadsadsadsadsadsad',
+    title: 'Typescript',
+    price: '9',
+    img:'https://blog.toothpickapp.com/content/images/2022/08/ts_logo.png',
+    quantity: 1,
+  },
+  {
+    id: 'sadsadsadsadsadsadsadsa',
+    title: 'Bootstrap 5',
+    price: '9',
+    img: 'https://www.etatvasoft.com/insights/wp-content/uploads/2017/02/b-thumb-img9.jpg',
+    quantity: 1,
+  },
+  {
+    id: 'adsadsadsadsadsadsadsaf',
+    title: 'React',
+    price: '9 ',
+    img: 'https://repository-images.githubusercontent.com/37153337/9d0a6780-394a-11eb-9fd1-6296a684b124',
+    quantity: 1,
+  },
+];
+
+export default courseItems;
